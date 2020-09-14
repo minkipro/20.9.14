74c0998dc1388d83d84324d26dfa0c43a49ec15c
ghkdalsrl222 <ghkdalsrl222@gmail.com>
2020-09-15 01:29:09 +0900

    directx11xtk 추가

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
-void main()
+#include <Windows.h>
+#pragma comment(lib,"d3d11.lib")
+#pragma comment(lib, "DirectXTK.lib")
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hProInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nCmdShow)
 {
-	int a;
-	a = 1;
+	return 0;
 }
